a71c104eb0e4f005a3879ba1f87bc464a7f8cb89
Antony Carrasco Cunya <coltmoonsac@gmail.com>
2023-10-02 16:54:40 -0500

    Excersise 1.7 done

--- part-1/unicafe/src/Statistics.jsx
+++ part-1/unicafe/src/Statistics.jsx
@@ -1,10 +1,16 @@
 const Statistics = ({good, bad, neutral}) => {
+  const all = good + bad + neutral;
+  const average = (good - bad) / all;
+  const positive = good / all * 100;  
   return (
     <>
       <h2>Statistics</h2>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>all: {all} </p>
+      <p>average: {average || 0}</p>
+      <p>positive: {positive} %</p>
     </>
   );
 }
